981ea6186b0c8a377f88065c5c23d0cf7ce14c0a
s1F102302865 <136315303+s1F102302865@users.noreply.github.com>
2025-10-22 16:18:51 +0900

    提案画面削除

--- drivey/views.py
+++ drivey/views.py
@@ -5,8 +5,3 @@ from django.http import HttpResponse
 # ログインページの表示
 def login(request):
     return render(request, 'drivey/login.html')
-
-
-# 提案画面の表示
-def suggest(request):
-    return render(request, 'drivey/suggest.html')
